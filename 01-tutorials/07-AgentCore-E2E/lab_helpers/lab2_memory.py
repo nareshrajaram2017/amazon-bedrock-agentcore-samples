c53d01047044606d5f4f53fe05d62b482555c3dc
Akarsha Sehwag <akshseh@amazon.de>
2025-08-11 19:10:01 -0400

    minor_update to memory

--- 01-tutorials/07-AgentCore-E2E/lab_helpers/lab2_memory.py
+++ 01-tutorials/07-AgentCore-E2E/lab_helpers/lab2_memory.py
@@ -7,14 +7,17 @@ import sys
 import uuid
 from typing import Dict
 
+import boto3
 from bedrock_agentcore.memory import MemoryClient
 from bedrock_agentcore.memory.constants import StrategyType
-
-from strands.hooks import AfterInvocationEvent, HookProvider, HookRegistry, MessageAddedEvent
-
-import boto3
 from boto3.session import Session
 from botocore.exceptions import ClientError
+from strands.hooks import (
+    AfterInvocationEvent,
+    HookProvider,
+    HookRegistry,
+    MessageAddedEvent,
+)
 
 boto_session = Session()
 REGION = boto_session.region_name
@@ -29,7 +32,8 @@ SESSION_ID = str(uuid.uuid4())
 memory_client = MemoryClient(region_name=REGION)
 memory_name = "CustomerSupportMemory"
 
-def create_memory_resource():
+
+def create_or_get_memory_resource():
     try:
         memory_id = get_ssm_parameter("/app/customersupport/agentcore/memory_id")
         memory_client.gmcp_client.get_memory(memoryId=memory_id)
@@ -57,7 +61,7 @@ def create_memory_resource():
                 name=memory_name,
                 description="Customer support agent memory",
                 strategies=strategies,
-                event_expiry_days=90,          # Memories expire after 90 days
+                event_expiry_days=90,  # Memories expire after 90 days
             )
             memory_id = response["id"]
             try:
@@ -68,15 +72,17 @@ def create_memory_resource():
         except:
             return None
 
+
 def delete_memory(memory_hook):
 
     try:
-        ssm_client = boto3.client('ssm', region_name=REGION)
+        ssm_client = boto3.client("ssm", region_name=REGION)
 
         memory_client.delete_memory(memory_id=memory_hook.memory_id)
-        ssm_client.delete_parameter(Name='/app/customersupport/agentcore/memory_id')
+        ssm_client.delete_parameter(Name="/app/customersupport/agentcore/memory_id")
     except Exception:
         pass
+
 
 class CustomerSupportMemoryHooks(HookProvider):
     """Memory hooks for customer support agent"""
