7fa5f71f33921f1a85df1b3f90c76af598291dd9
HT <hardik.thakkar00@gmail.com>
2025-08-13 23:01:42 +0000

    Rename, Refactor and deletion
    Added sagemaker_helper

--- 01-tutorials/07-AgentCore-E2E/lab_helpers/lab4_runtime.py
+++ 01-tutorials/07-AgentCore-E2E/lab_helpers/lab4_runtime.py
@@ -1,41 +1,35 @@
 from bedrock_agentcore.runtime import (
     BedrockAgentCoreApp,
-)  # ## AGENTCORE RUNTIME - LINE 1 ###
-from lab_helpers.lab1_strands_agent import (
-    SYSTEM_PROMPT,
-    get_product_info,
-    get_return_policy,
-)
-from lab_helpers.lab1_guardrails import create_or_get_guardrail_resource
-from lab_helpers.lab2_memory import (
-    ACTOR_ID,
-    SESSION_ID,
-    CustomerSupportMemoryHooks,
-    create_or_get_memory_resource,
-    memory_client,
-)
+)  #### AGENTCORE RUNTIME - LINE 1 ####
 from strands import Agent
 from strands.models import BedrockModel
+from scripts.utils import get_ssm_parameter
+from lab_helpers.lab1_strands_agent import (
+    get_return_policy,
+    get_product_info,
+    SYSTEM_PROMPT,
+    MODEL_ID,
+)
 
-# Lab1 import: Get Bedrock Guardrail
-guardrail_id, guardrail_version = create_or_get_guardrail_resource()
+from lab_helpers.lab2_memory import (
+    CustomerSupportMemoryHooks,
+    memory_client,
+    ACTOR_ID,
+    SESSION_ID,
+)
 
 # Lab1 import: Create the Bedrock model
-model_id = "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
 model = BedrockModel(
-    model_id=model_id,
-    guardrail_id=guardrail_id,
-    # guardrail_version=guardrail_version,
-    guardrail_trace="enabled",
+    model_id=MODEL_ID
 )
 
 # Lab2 import : Initialize memory via hooks
-memory_id = create_or_get_memory_resource()
+memory_id = get_ssm_parameter("/app/customersupport/agentcore/memory_id")
 memory_hooks = CustomerSupportMemoryHooks(
     memory_id, memory_client, ACTOR_ID, SESSION_ID
 )
 
-# Lab1 import: Create the agent with all customer support tools
+# Create the agent with all customer support tools
 agent = Agent(
     model=model,
     tools=[get_return_policy, get_product_info],
@@ -44,10 +38,10 @@ agent = Agent(
 )
 
 # Initialize the AgentCore Runtime App
-app = BedrockAgentCoreApp()  ### AGENTCORE RUNTIME - LINE 2 ###
+app = BedrockAgentCoreApp()  #### AGENTCORE RUNTIME - LINE 2 ####
 
 
-@app.entrypoint  ### AGENTCORE RUNTIME - LINE 3 ###
+@app.entrypoint  #### AGENTCORE RUNTIME - LINE 3 ####
 def invoke(payload):
     """AgentCore Runtime entrypoint function"""
     user_input = payload.get("prompt", "")
@@ -58,4 +52,4 @@ def invoke(payload):
 
 
 if __name__ == "__main__":
-    app.run()  ### AGENTCORE RUNTIME - LINE 4 ###
+    app.run()  #### AGENTCORE RUNTIME - LINE 4 ####
